c10e2536208fdc91198e8704213a3afa54c7d902
Usman Shakhzad <sheol.9898@ukr.net>
2024-06-18 19:11:15 +0300

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { MovieListComponent } from "./components/movie-list/movie-list.component";
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [MovieListComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [MovieListComponent],
 })
 export class AppComponent {
   title = 'angular-course';
